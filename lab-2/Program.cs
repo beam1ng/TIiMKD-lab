300a52c11c3d518422303d066fbc6bb310e9cb41
Kuba <jakub.augustyniak1.618@gmail.com>
2023-03-23 10:26:59 +0100

    Implemented first task of the second lab.

---
+++ lab-2/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using lab_2;
+
+string solutionDir = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory())));
+string relativeFilePath = Path.Combine(solutionDir, "src", "norm_wiki_sample.txt");
+var wikiText = File.ReadAllText(relativeFilePath);
+var textWords = wikiText.Split(' ');
+var wordsCount = textWords.Length;
+var wordsCounts = Utility.CalculateWordCounts(textWords);
+var sortedWordsCounts = wordsCounts.OrderByDescending(pair=>pair.Value);
+var first30k = Utility.GetSubDictionary(sortedWordsCounts,30000);
+var first30kFraction = first30k.Sum(pair => pair.Value) / (float)wordsCount;
+var first6k = Utility.GetSubDictionary(sortedWordsCounts,6000);
+var first6kFraction = first6k.Sum(pair => pair.Value) / (float)wordsCount;
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Word: '{sortedWordsCounts.ElementAt(i).Key}', Frequency: {sortedWordsCounts.ElementAt(i).Value/(float)wordsCount}");
+}
+Console.WriteLine($"First 30k fraction = {first30kFraction}");
+Console.WriteLine($"First 6k fraction = {first6kFraction}");
---
+++ lab-2/Utility.cs
@@ -0,0 +1,27 @@
+namespace lab_2;
+
+public static class Utility
+{
+    public static Dictionary<string, int> CalculateWordCounts(string[] textWords)
+    {
+        Dictionary<string, int> wordFrequencies = new Dictionary<string, int>();
+        foreach (var t in textWords)
+        {
+            if (wordFrequencies.ContainsKey(t))
+            {
+                wordFrequencies[t]++;
+            }
+            else
+            {
+                wordFrequencies.Add(t,1);
+            }
+        }
+
+        return wordFrequencies;
+    }
+
+    public static IOrderedEnumerable<KeyValuePair<string, int>> GetSubDictionary(IOrderedEnumerable<KeyValuePair<string, int>> original,int firstN)
+    {
+        return original.Take(firstN).OrderByDescending(pair=>pair.Value);
+    }
+}

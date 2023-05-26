c765356170dbc2415d82db565324c43fa9e761f2
Jakub <jakub.augustyniak1.618@gmail.com>
2023-05-26 02:20:48 +0200

    Completed lab 3 tasks.

--- lab-3/Program.cs
+++ lab-3/Program.cs
@@ -1,59 +1,68 @@
-﻿using lab_3;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using lab_3;
 
-var solutionDir = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory())));
-var relativeFilePath = Path.Combine(solutionDir, "src", "norm_wiki_en.txt");
-var inputText = File.ReadAllText(relativeFilePath);
-
-//character entropy
-var characterEntropy_Deg0 = Utility.CalculateCharacterEntropy(0);
-var characterEntropy_Deg1 = Utility.CalculateCharacterEntropy(1);
-var characterEntropy_Deg2 = Utility.CalculateCharacterEntropy(2);
-var characterEntropy_Deg3 = Utility.CalculateCharacterEntropy(3);
-var characterEntropy_Deg4 = Utility.CalculateCharacterEntropy(4);
-
-//word entropy
-var wordEntropy_Deg0 = Utility.CalculateWordEntropy(0);
-var wordEntropy_Deg1 = Utility.CalculateWordEntropy(1);
-var wordEntropy_Deg2 = Utility.CalculateWordEntropy(2);
-var wordEntropy_Deg3 = Utility.CalculateWordEntropy(3);
-var wordEntropy_Deg4 = Utility.CalculateWordEntropy(4);
-
-//results
-enum EntropyType
+namespace Lab_3
 {
-    WORD,
-    CHARACTER
-}
-
-struct EntropyData
-{
-    public EntropyData(EntropyType entropyType, int entropyDegree, float entropyValue)
+    class Program
     {
-        this.entropyType = entropyType;
-        this.entropyDegree = entropyDegree;
-        this.entropyValue = entropyValue;
+        enum EntropyType
+        {
+            WORD,
+            CHARACTER
+        }
+
+        struct EntropyData
+        {
+            public EntropyData(EntropyType entropyType, int entropyDegree, float entropyValue)
+            {
+                this.entropyType = entropyType;
+                this.entropyDegree = entropyDegree;
+                this.entropyValue = entropyValue;
+            }
+
+            public EntropyType entropyType;
+            public int entropyDegree;
+            public float entropyValue;
+        }
+
+        static void Main(string[] args)
+        {
+            var solutionDir = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory())));
+            var relativeFilePath = Path.Combine(solutionDir, "src", "sample5.txt");
+            var inputText = File.ReadAllText(relativeFilePath);
+            
+            var characterEntropy_Deg0 = Utility.CalculateCharacterEntropy(inputText, 0);
+            var characterEntropy_Deg1 = Utility.CalculateCharacterEntropy(inputText, 1);
+            var characterEntropy_Deg2 = Utility.CalculateCharacterEntropy(inputText, 2);
+            var characterEntropy_Deg3 = Utility.CalculateCharacterEntropy(inputText, 3);
+            var characterEntropy_Deg4 = Utility.CalculateCharacterEntropy(inputText, 4);
+            
+            var wordEntropy_Deg0 = Utility.CalculateWordEntropy(inputText, 0);
+            var wordEntropy_Deg1 = Utility.CalculateWordEntropy(inputText, 1);
+            var wordEntropy_Deg2 = Utility.CalculateWordEntropy(inputText, 2);
+            var wordEntropy_Deg3 = Utility.CalculateWordEntropy(inputText, 3);
+            var wordEntropy_Deg4 = Utility.CalculateWordEntropy(inputText, 4);
+            
+            var entropyDatas = new List<EntropyData>()
+            {
+                new EntropyData(EntropyType.CHARACTER, 0, characterEntropy_Deg0),
+                new EntropyData(EntropyType.CHARACTER, 1, characterEntropy_Deg1),
+                new EntropyData(EntropyType.CHARACTER, 2, characterEntropy_Deg2),
+                new EntropyData(EntropyType.CHARACTER, 3, characterEntropy_Deg3),
+                new EntropyData(EntropyType.CHARACTER, 4, characterEntropy_Deg4),
+                new EntropyData(EntropyType.WORD, 0, wordEntropy_Deg0),
+                new EntropyData(EntropyType.WORD, 1, wordEntropy_Deg1),
+                new EntropyData(EntropyType.WORD, 2, wordEntropy_Deg2),
+                new EntropyData(EntropyType.WORD, 3, wordEntropy_Deg3),
+                new EntropyData(EntropyType.WORD, 4, wordEntropy_Deg4)
+            };
+            
+            foreach (var entropyData in entropyDatas)
+            {
+                Console.WriteLine($"Type: {entropyData.entropyType}, Degree: {entropyData.entropyDegree}, Entropy: {entropyData.entropyValue}");
+            }
+        }
     }
-
-    public EntropyType entropyType;
-    public int entropyDegree;
-    public float entropyValue;
-}
-
-var entropyDatas = new List<EntropyData>()
-{
-    new EntropyData(EntropyType.CHARACTER, 0, characterEntropy_Deg0),
-    new EntropyData(EntropyType.CHARACTER, 1, characterEntropy_Deg1),
-    new EntropyData(EntropyType.CHARACTER, 2, characterEntropy_Deg2),
-    new EntropyData(EntropyType.CHARACTER, 3, characterEntropy_Deg3),
-    new EntropyData(EntropyType.CHARACTER, 4, characterEntropy_Deg4),
-    new EntropyData(EntropyType.WORD, 0, wordEntropy_Deg0),
-    new EntropyData(EntropyType.WORD, 1, wordEntropy_Deg1),
-    new EntropyData(EntropyType.WORD, 2, wordEntropy_Deg2),
-    new EntropyData(EntropyType.WORD, 3, wordEntropy_Deg3),
-    new EntropyData(EntropyType.WORD, 4, wordEntropy_Deg4)
-};
-
-foreach (var entropyData in entropyDatas)
-{
-    Console.Out.WriteLine($"Type: {entropyData.entropyType}, Degree:{entropyData.entropyDegree}, Entropy: {entropyData.entropyValue}");
 }
--- lab-3/Utility.cs
+++ lab-3/Utility.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace lab_3;
 
 public class Utility
@@ -6,15 +8,60 @@ public class Utility
 
     public static float CalculateCharacterEntropy(string inputText, int degree)
     {
-        var sequencesToFrequencies = GetSequencesToCharFrequencies(inputText, degree);
-        var entropy = CalculateCharacterEntropy(sequencesToFrequencies, degree);
+        var sequencesToCharFrequencies = GetSequencesToCharFrequencies(inputText, degree);
+        var sequencesFrequencies = CalculateSequencesFrequencies(inputText, degree);
+        var entropy = CalculateCharacterEntropy(sequencesToCharFrequencies,sequencesFrequencies, degree);
+        return entropy;
     }
     
     public static float CalculateWordEntropy(string inputText, int degree)
     {
-        
+        var sequencesToSequencesFrequencies = GetSequencesToStringFrequencies(inputText, degree);
+        var sequencesFrequencies = CalculateWordSequencesFrequencies(inputText, degree);
+        var entropy = CalculateWordEntropy(sequencesToSequencesFrequencies,sequencesFrequencies,CalculateUniqueWordsCount(inputText), degree);
+        return entropy;
     }
-    
+
+    public static Dictionary<string, Dictionary<string, float>> GetSequencesToStringFrequencies(string text, int sequenceWordCount)
+    {
+        var sequencesToSequenceFrequencies = new Dictionary<string, Dictionary<string, float>>();
+        var sequencesToSequenceCounts = new Dictionary<string, Dictionary<string, int>>();
+        var textWords = text.Split();
+        var textWordsLength = textWords.Length;
+
+        for (var i = sequenceWordCount; i < textWordsLength; i++)
+        {
+            var sequence = textWords.Skip(i - sequenceWordCount).Take(sequenceWordCount).ToList();
+            var stringifiedSequence = string.Join(" ", sequence);
+            
+            if (!sequencesToSequenceCounts.ContainsKey(stringifiedSequence))
+            {
+                sequencesToSequenceCounts.Add(stringifiedSequence, new Dictionary<string, int>());
+                sequencesToSequenceFrequencies.Add(stringifiedSequence, new Dictionary<string, float>());
+                sequencesToSequenceCounts[stringifiedSequence].Add(textWords[i], 1);
+            }
+            else
+            {
+                if (!sequencesToSequenceCounts[stringifiedSequence].ContainsKey(textWords[i]))
+                    sequencesToSequenceCounts[stringifiedSequence].Add(textWords[i], 1);
+                else
+                    sequencesToSequenceCounts[stringifiedSequence][textWords[i]]++;
+            }
+        }
+
+        foreach (var sequenceToCharCount in sequencesToSequenceCounts)
+        {
+            var currentSequence = sequenceToCharCount.Key;
+            var totalSequenceCount = sequenceToCharCount.Value.Sum(pair => pair.Value);
+            foreach (var charFrequency in sequencesToSequenceCounts[currentSequence])
+                sequencesToSequenceFrequencies[currentSequence].Add(
+                    charFrequency.Key,
+                    sequencesToSequenceCounts[currentSequence][charFrequency.Key] / (float)totalSequenceCount);
+        }
+
+        return sequencesToSequenceFrequencies;
+    }
+
     public static Dictionary<string, Dictionary<char, float>> GetSequencesToCharFrequencies(string text, int length)
     {
         var sequencesToCharFrequencies = new Dictionary<string, Dictionary<char, float>>();
@@ -49,40 +96,18 @@ public class Utility
 
         return sequencesToCharFrequencies;
     }
-    
-    public static Dictionary<char, float> CalculateCharactersFrequencies(string text, string prefix)
-    {
-        var charsToFrequencies = new Dictionary<char, float>();
-        var charsCounts = new Dictionary<char, int>();
 
-        var validCharsCount = 0f;
-
-        for (var i = 0; i < possibleChars.Length; i++)
-        {
-            charsToFrequencies.Add(possibleChars[i], 0f);
-            charsCounts.Add(possibleChars[i], 0);
-        }
-
-        for (var i = prefix.Length; i < text.Length; i++)
-            if (text.Substring(i - prefix.Length, prefix.Length) == prefix)
-            {
-                charsCounts[text[i]]++;
-                validCharsCount++;
-            }
-
-        for (var i = 0; i < possibleChars.Length; i++)
-            charsToFrequencies[possibleChars[i]] = charsCounts[possibleChars[i]] / validCharsCount;
-
-        return charsToFrequencies;
-    }
-    
-    public float CalculateCharacterEntropy(Dictionary<string,Dictionary<char, float>> sequencesToCharactersFrequencies, int degree)
+    public static float CalculateCharacterEntropy(Dictionary<string,Dictionary<char, float>> sequencesToCharactersFrequencies,Dictionary<string,float> sequencesFrequencies, int degree)
     {
         int possibleOutputsCount = possibleChars.Length;
         float entropy = 0.0f;
         foreach (var sequenceCharactersFrequencies in sequencesToCharactersFrequencies)
         {
-            var sequenceFrequency = CalculateSequenceFrequency(sequenceCharactersFrequencies.Key);
+            if (!sequencesFrequencies.ContainsKey(sequenceCharactersFrequencies.Key))
+            {
+                continue;
+            }
+            var sequenceFrequency = sequencesFrequencies[sequenceCharactersFrequencies.Key];
             var sequenceEntropy = 0.0f;
             
             foreach (var characterFrequency in sequenceCharactersFrequencies.Value)
@@ -93,19 +118,120 @@ public class Utility
             entropy += sequenceFrequency * sequenceEntropy;
         }
         
-        return entropy;
+        return -1*entropy;
     }
-
-    private float CalculateSequencesFrequencies(int length, string text)
+    
+    public static float CalculateWordEntropy(Dictionary<string,Dictionary<string, float>> sequencesToSequencesFrequencies,Dictionary<string,float> sequencesFrequencies,int possibleWordsCount, int degree)
     {
-        //this is total rubbish, to be replaced todo:<=
-        var allSequences = new List<string>();
-        for (var i = length; i < text.Length; i++)
+        int possibleOutputsCount = possibleWordsCount;
+        float entropy = 0.0f;
+        foreach (var sequenceSequencesFrequencies in sequencesToSequencesFrequencies)
         {
-            var currentSequence = text.Substring(i - length, length);
-            if (!allSequences.Contains(currentSequence)) allSequences.Add(currentSequence);
+            if (!sequencesFrequencies.ContainsKey(sequenceSequencesFrequencies.Key))
+            {
+                continue;
+            }
+            var sequenceFrequency = sequencesFrequencies[sequenceSequencesFrequencies.Key];
+            var sequenceEntropy = 0.0f;
+            
+            foreach (var sequenceSequenceFrequency in sequenceSequencesFrequencies.Value)
+            {
+                sequenceEntropy += sequenceSequenceFrequency.Value * MathF.Log(sequenceSequenceFrequency.Value, (float) possibleOutputsCount);
+            }
+            
+            entropy += sequenceFrequency * sequenceEntropy;
+        }
+        
+        return -1*entropy;
+    }
+    
+    private static Dictionary<string,float> CalculateSequencesFrequencies(string text,int degree)
+    {
+        var allSequencesCounts = new Dictionary<string,int>();
+        for (var i = degree; i < text.Length; i++)
+        {
+            var currentSequence = text.Substring(i - degree, degree);
+            if (!allSequencesCounts.ContainsKey(currentSequence))
+            {
+                allSequencesCounts.Add(currentSequence,1);
+            }
+            else
+            {
+                allSequencesCounts[currentSequence]++;
+            }
         }
 
-        return allSequences;
+        int totalSequencesCount = text.Length + 1 - degree;
+        Dictionary<string, float> sequencesFrequencies = new Dictionary<string, float>();
+        foreach (var sequencesCount in allSequencesCounts)
+        {
+            sequencesFrequencies.Add(sequencesCount.Key,(float)sequencesCount.Value/totalSequencesCount);
+        }
+
+        return sequencesFrequencies;
+    }
+    
+    private static Dictionary<string,float> CalculateWordSequencesFrequencies(string text,int degree)
+    {
+        if (degree == 0)
+        {
+            var output = new Dictionary<string, float>();
+            output.Add("",1.0f);
+            return output;
+        }
+        var allStringifiedSequencesCounts = new Dictionary<string,int>();
+        var splitText = text.Split(" ");
+        for (var i = degree; i < text.Length; i++)
+        {
+            var currentSequence = splitText.Skip(i - degree).Take(degree).ToList();
+            var stringifiedSequence = string.Join(" ", currentSequence);
+            
+            if (!allStringifiedSequencesCounts.ContainsKey(stringifiedSequence))
+            {
+                allStringifiedSequencesCounts.Add(stringifiedSequence,1);
+            }
+            else
+            {
+                allStringifiedSequencesCounts[stringifiedSequence]++;
+            }
+        }
+
+        int totalWordsCount = splitText.Length + 1 - degree;
+        Dictionary<string, float> sequencesFrequencies = new Dictionary<string, float>();
+        foreach (var sequencesCount in allStringifiedSequencesCounts)
+        {
+            sequencesFrequencies.Add(sequencesCount.Key,(float)sequencesCount.Value/totalWordsCount);
+        }
+
+        return sequencesFrequencies;
+    }
+
+    private static int CalculateUniqueWordsCount(string text)
+    {
+        HashSet<string> uniqueWords = new HashSet<string>();
+        StringBuilder currentWord = new StringBuilder();
+
+        foreach (char c in text)
+        {
+            if (char.IsWhiteSpace(c))
+            {
+                if (currentWord.Length > 0)
+                {
+                    uniqueWords.Add(currentWord.ToString());
+                    currentWord.Clear();
+                }
+            }
+            else
+            {
+                currentWord.Append(c);
+            }
+        }
+
+        if (currentWord.Length > 0)
+        {
+            uniqueWords.Add(currentWord.ToString());
+        }
+
+        return uniqueWords.Count;
     }
 }
